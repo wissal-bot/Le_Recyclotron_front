760aa9a92afd0bfc4316880cd5dc1cabd8aadc6a
Nchanti33 <nchanti33nchanti@gmail.com>
2025-06-13 11:45:00 +0200

    martin voulais que je push

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@
  */
 export const environment = Object.freeze({
   production: false,
-  API_URL: 'http://localhost:3000/api',
+  API_URL: 'http://localhost:3000',
   // Add additional environment variables as needed
 });
