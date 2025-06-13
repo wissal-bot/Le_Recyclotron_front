a63e21799c34c3227b6800b536c545f7d91dae67
Nchanti33 <nchanti33nchanti@gmail.com>
2025-06-13 12:35:12 +0200

    martin pouvais pas commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,5 @@
  */
 export const environment = Object.freeze({
   production: true,
-  API_URL: process.env['API_URL'], // Replace with actual production URL
-  // Add additional environment variables as needed
+  API_URL: 'frontend/api',
 });
